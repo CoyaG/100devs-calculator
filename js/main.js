79afc0d3ed80007f2ef703aa34126607d3431428
CoyaG <108843720+CoyaG@users.noreply.github.com>
2023-04-09 13:43:06 -0400

    Added updated files

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/* What does a calculator need to do?
+1. Accept user inputs
+2. Should accept decimal numbers
+3. Store those inputs
+4. Recognize inputs and perform calculations
+5. Return a result 
+
+Extras:
+- Display all input as it is being entered
+- Store previous total as start of next operation
+- Functional clear button */
+
