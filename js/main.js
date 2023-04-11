3aeb1e9acea4a3215974ee5af8949c0bf32bc02e
CoyaG <108843720+CoyaG@users.noreply.github.com>
2023-04-10 20:20:19 -0400

    Updated all files, style, and js

--- js/main.js
+++ js/main.js
@@ -8,5 +8,30 @@
 Extras:
 - Display all input as it is being entered
 - Store previous total as start of next operation
-- Functional clear button */
+- Functional clear button
+- Prevent invalid inputs (operators next to each other, etc.) */
 
+const keys = document.querySelector('.calc-buttons');//doing this targets all buttons
+  keys.addEventListener('click', event => {
+    const {target} = event;
+    const {value} = target;
+    if(!target.matches('button')){
+      return; //want to pass nothing; exit; not necessary
+    }else{
+      //putting nothing here ensure that when you click outside of a button, nothing happens
+      //console.log(target) --> this is for testing stuff out
+    }
+  })
+
+const calculator = {
+  displayText: '0',
+  prevTotal: null,
+
+  //method:
+  parseInput(value){
+    if(this.displayText === '0'){
+      this.displayText = ''
+    }
+    
+  }
+}
