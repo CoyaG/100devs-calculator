18293287937fe41846bfa964e997984c66285b59
CoyaG <108843720+CoyaG@users.noreply.github.com>
2023-04-11 20:11:44 -0400

    Updated main js file

--- js/main.js
+++ js/main.js
@@ -11,27 +11,52 @@ Extras:
 - Functional clear button
 - Prevent invalid inputs (operators next to each other, etc.) */
 
-const keys = document.querySelector('.calc-buttons');//doing this targets all buttons
-  keys.addEventListener('click', event => {
-    const {target} = event;
-    const {value} = target;
-    if(!target.matches('button')){
-      return; //want to pass nothing; exit; not necessary
-    }else{
-      //putting nothing here ensure that when you click outside of a button, nothing happens
-      //console.log(target) --> this is for testing stuff out
-    }
-  })
+const keys = document.querySelector('.calc-buttons'); //doing this targets all buttons
+keys.addEventListener('click', (event) => {
+  const { target } = event;
+  const { value } = target;
+  if (!target.matches('button')) {
+    return; //want to pass nothing; exit; not necessary
+  } else {
+    //putting nothing here ensure that when you click outside of a button, nothing happens
+    //console.log(target) --> this is for testing stuff out
+  }
+});
 
 const calculator = {
   displayText: '0',
   prevTotal: null,
 
-  //method:
-  parseInput(value){
-    if(this.displayText === '0'){
-      this.displayText = ''
+  //method
+  parseInput(value) {
+    //have any of the non-numeral buttons been clicked?
+    switch (value) {
+      case '=':
+        //calculate the answer
+        break;
+      case 'Clear':
+        //clear screen and all stored values
+        break;
+      case '.':
+        if (this.displayText == 0) {
+          //pass '0.' into add text method
+        } else {
+          //add value to text string
+        }
+        break;
+      default:
+        //add value to text string
+        break;
     }
-    
-  }
-}
+  },
+  //method
+  addText(value) {
+    if (this.displayText === '0') {
+      this.displayText = '';
+    }else if(this.prevTotal !== null){
+      //this takes 3+3=6 then +3=9, for example
+      this.displayText = this.prevTotal
+      this.prevTotal = null
+    }
+  },
+};
